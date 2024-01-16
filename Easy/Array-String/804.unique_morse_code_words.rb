43b361b7d890ff3fd7472b751fd357f1dcb586b9
Vishal Sadriya <vishalsadriya.tps@gmail.com>
2024-01-16 22:43:29 +0100

    804

---
+++ Easy/Array-String/804.unique_morse_code_words.rb
@@ -0,0 +1,45 @@
+# International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+# 'a' maps to ".-",
+# 'b' maps to "-...",
+# 'c' maps to "-.-.", and so on.
+# For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+# [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+# Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+# For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+# Return the number of different transformations among all words we have.
+
+MORSE_CODE_ARRAY = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+MORSE_CODE_HASH = Hash[('a'..'z').to_a.zip(MORSE_CODE_ARRAY)]
+def unique_morse_representations(words)
+  result = []
+  words.each do |word|
+    current_str = ''
+    word.each_char do |c|
+      current_str += MORSE_CODE_HASH[c]
+    end
+    result << current_str
+  end
+
+  result.uniq.size
+end
+
+# Example 1:
+
+# Input: words = ["gin","zen","gig","msg"]
+# Output: 2
+# Explanation: The transformation of each word is:
+# "gin" -> "--...-."
+# "zen" -> "--...-."
+# "gig" -> "--...--."
+# "msg" -> "--...--."
+# There are 2 different transformations: "--...-." and "--...--.".
+# Example 2:
+
+# Input: words = ["a"]
+# Output: 1
+# words = ["gin","zen","gig","msg"]
+words = ["a"]
+p unique_morse_representations(words)
