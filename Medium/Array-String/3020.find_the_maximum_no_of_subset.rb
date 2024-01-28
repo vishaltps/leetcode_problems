433e343cc3961c95d6481b70543cf23512d52b05
Vishal Sadriya <vishalsadriya.tps@gmail.com>
2024-01-28 15:09:06 +0100

    3020

---
+++ Medium/Array-String/3020.find_the_maximum_no_of_subset.rb
@@ -0,0 +1,52 @@
+# You are given an array of positive integers nums.
+
+# You need to select a 
+# subset
+#  of nums which satisfies the following condition:
+
+# You can place the selected elements in a 0-indexed array such that it follows the pattern: [x, x2, x4, ..., xk/2, xk, xk/2, ..., x4, x2, x] (Note that k can be be any non-negative power of 2). For example, [2, 4, 16, 4, 2] and [3, 9, 3] follow the pattern while [2, 4, 8, 4, 2] does not.
+# Return the maximum number of elements in a subset that satisfies these conditions.
+
+def maximum_length(nums)
+  h = nums.tally
+  count = h.size > 0 ? 1 : 0
+  count = h[1].odd? ? h[1] : h[1] - 1 if h[1]
+
+  h.each do |k, v|
+    next count if k == 1 || v <= 1
+
+    next unless h.key?(k**2)
+
+    pow = 2
+    current_count = 2
+    while h.key?(k**pow) && h[k**pow] > 1
+      current_count += 1
+      pow *= 2
+      current_count += 1 if h.key?(k**pow)
+    end
+
+    current_count += 1 if h.key?(k**pow)
+    count = [current_count, count].max
+  end
+  count
+end
+
+# Example 1:
+
+# Input: nums = [5,4,1,2,2]
+# Output: 3
+# Explanation: We can select the subset {4,2,2}, which can be placed in the array as [2,4,2] which follows the pattern and 22 == 4. Hence the answer is 3.
+# Example 2:
+
+# Input: nums = [1,3,2,4]
+# Output: 1
+# Explanation: We can select the subset {1}, which can be placed in the array as [1] which follows the pattern. Hence the answer is 1. Note that we could have also selected the subsets {2}, {4}, or {3}, there may be multiple subsets which provide the same answer. 
+
+# nums = [5,4,1,2,2]
+# 3
+nums = [1,16,49,16,121]
+# 1
+nums = [4,36,81,16,25]
+# 1
+
+p maximum_length(nums)
