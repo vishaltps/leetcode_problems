bbd7328519d196a18920111e08d7f444fccb93a5
Vishal Sadriya <vishalsadriya.tps@gmail.com>
2024-02-01 07:42:03 +0100

    4

--- Hard/Binary-Search/4.mediun_of_two_sorted_array.rb
+++ Hard/Binary-Search/4.mediun_of_two_sorted_array.rb
@@ -3,14 +3,44 @@
 # The overall run time complexity should be O(log (m+n)).
 
 def find_median_sorted_arrays(nums1, nums2)
-  merged_arry = (nums1 + nums2).sort
-  mid = merged_arry.length / 2
-  if(merged_arry.length % 2 == 0)
-      (merged_arry[mid - 1] + merged_arry[mid]) / 2.to_f
-  else
-      merged_arry[mid].to_f
-  end
+  # merged_arry = (nums1 + nums2).sort
+  # mid = merged_arry.length / 2
+  # if(merged_arry.length % 2 == 0)
+  #     (merged_arry[mid - 1] + merged_arry[mid]) / 2.to_f
+  # else
+  #     merged_arry[mid].to_f
+  # end
   
+  n1, n2 = nums1.size, nums2.size
+  
+  return find_median_sorted_arrays(nums2, nums1) if n1 > n2
+
+  low, high = 0, n1
+  left = (n1 + n2 + 1) / 2
+  n = n1 + n2
+
+  while low <= high
+    mid1 = (low + high) >> 1
+    mid2 = left - mid1
+    l1, l2, r1, r2 = -Float::INFINITY, -Float::INFINITY, Float::INFINITY, Float::INFINITY
+
+    l1 = nums1[mid1 - 1] if mid1 - 1 >= 0
+    l2 = nums2[mid2 - 1] if mid2 - 1 >= 0
+    r1 = nums1[mid1] if mid1 < n1
+    r2 = nums2[mid2] if mid2 < n2
+
+    if l1 <= r2 && l2 <= r1
+      if n % 2 == 1
+        return [l1, l2].max.to_f
+      else
+        return (([l1, l2].max + [r1, r2].min) / 2.0)
+      end
+    elsif l1 > r2
+      high = mid1 - 1
+    else
+      low = mid1 + 1
+    end
+  end
 end
 
 # Example 1:
@@ -23,3 +53,11 @@ end
 # Input: nums1 = [1,2], nums2 = [3,4]
 # Output: 2.50000
 # Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+# nums1 = [1,3]
+#  nums2 = [2]
+nums1 = [1,2]
+ nums2 = [3,4]
+# nums1 = [2,4,6]
+# nums2 = [1,3,5]
+p find_median_sorted_arrays(nums1, nums2)
